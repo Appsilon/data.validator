4f90f83c9d3c3f5e8f5bb7f4ea06e301f08a1a80
Krystian Igras <krystian8207@gmail.com>
2020-10-06 14:50:08 +0200

    Add assert_rows and assert_cols.

--- R/assertions.R
+++ R/assertions.R
@@ -25,11 +25,55 @@ validate <- function(data, name, description = NULL, validator = getDefaultValid
 }
 
 #' @export
-assert_if <- function(data, expr, success_fun = success_continue, error_fun = error_stop,
+assert_if <- function(data, expr, success_fun = success_append, error_fun = error_append,
                       skip_chain_opts = FALSE, obligatory = FALSE, defect_fun = defect_append,
                       description = NA) {
   assertr::verify(data = data, expr = !!rlang::enexpr(expr), success_fun = success_fun, error_fun = error_fun,
                   skip_chain_opts = skip_chain_opts, obligatory = obligatory, defect_fun = defect_fun,
                   description = description) %>%
     add_results(validator = attr(data, "data-validator"), attr(data, "data-name"))
+}
+
+#' @export
+assert_cols <- function(data, predicate, ..., col_based = FALSE, success_fun = success_append,
+                        error_fun = error_append, skip_chain_opts = FALSE, obligatory = FALSE,
+                        defect_fun = defect_append, description = NA) {
+  if (col_based) {
+    data <- assertr::insist(
+      data = data, predicate_generator = predicate, ...,
+      success_fun = success_fun, error_fun = error_fun,
+      skip_chain_opts = skip_chain_opts, obligatory = obligatory, defect_fun = defect_fun,
+      description = description
+    )
+  } else {
+    data <- assertr::assert(
+      data = data, predicate = predicate, ...,
+      success_fun = success_fun, error_fun = error_fun,
+      skip_chain_opts = skip_chain_opts, obligatory = obligatory, defect_fun = defect_fun,
+      description = description
+    )
+  }
+  add_results(data, validator = attr(data, "data-validator"), attr(data, "data-name"))
+}
+
+#' @export
+assert_rows <- function(data, row_reduction_fn, predicate, ..., col_based = FALSE, success_fun = success_append,
+                        error_fun = error_append, skip_chain_opts = FALSE, obligatory = FALSE,
+                        defect_fun = defect_append, description = NA) {
+  if (col_based) {
+    data <- assertr::insist_rows(
+      data = data, row_reduction_fn = row_reduction_fn, predicate_generator = predicate, ...,
+      success_fun = success_fun, error_fun = error_fun,
+      skip_chain_opts = skip_chain_opts, obligatory = obligatory, defect_fun = defect_fun,
+      description = description
+    )
+  } else {
+    data <- assertr::assert_rows(
+      data = data, row_reduction_fn = row_reduction_fn, predicate = predicate, ...,
+      success_fun = success_fun, error_fun = error_fun,
+      skip_chain_opts = skip_chain_opts, obligatory = obligatory, defect_fun = defect_fun,
+      description = description
+    )
+  }
+  add_results(data, validator = attr(data, "data-validator"), attr(data, "data-name"))
 }
--- examples/new_api/example.R
+++ examples/new_api/example.R
@@ -2,38 +2,33 @@ library(dplyr)
 library(assertr)
 library(data.validator)
 
-validate(mtcars) %>%
-  assert_if(description = "Column drat has only values larger than 3", drat > 3)
-
-validate(mtcars) %>%
-  assert(description = "No NA's inside mpg:carb columns", not_na, mpg:carb) %>%
-  assert(description = "vs and am values equal 0 or 2 only", in_set(c(0, 2)), vs, am) %>%
-  assert(description = "vs and am values should equal 3 or 4", skip_chain_opts = TRUE,
+validate(mtcars, description = "Motor Trend Car Road Tests") %>%
+  assert_cols(description = "No NA's inside mpg:carb columns", not_na, mpg:carb) %>%
+  assert_cols(description = "vs and am values equal 0 or 2 only", in_set(c(0, 2)), vs, am) %>%
+  assert_cols(description = "vs and am values should equal 3 or 4", skip_chain_opts = TRUE,
          error_fun = warning_append, in_set(c(3, 4)), gear, carb) %>%
   assert_rows(description = "Each row sum for am:vs columns is less or equal 2", rowSums, within_bounds(0, 2), vs:am) %>%
   assert_rows(description = "Each row sum for am:vs columns is less or equal 1", rowSums, within_bounds(0, 1), vs:am) %>%
-  insist(description = "For wt and qsec we have: abs(col) < 4 * sd(col)", within_n_sds(4), wt, qsec) %>%
-  insist(description = "For wt and qsec we have: abs(col) < 2 * sd(col)", within_n_sds(2), wt, qsec) %>%
-  insist_rows(description = "Using mpg:carb mahalanobis distance for each observation is within 30 median absolute deviations from the median",
-              maha_dist, within_n_mads(30), mpg:carb) %>%
-  insist_rows(description = "Using mpg:carb mahalanobis distance for each observation is within 3 median absolute deviations from the median",
-              maha_dist, within_n_mads(3), mpg:carb) %>%
-  verify(description = "Column drat has only positive values", drat > 0) %>%
-  verify(description = "Column drat has only values larger than 3", drat > 3) %>%
-  chain_end(error_fun = error_append) %>%
-  add_results(validator)
+  assert_cols(description = "For wt and qsec we have: abs(col) < 4 * sd(col)", within_n_sds(4), wt, qsec, col_based = TRUE) %>%
+  assert_cols(description = "For wt and qsec we have: abs(col) < 2 * sd(col)", within_n_sds(2), wt, qsec, col_based = TRUE) %>%
+  assert_rows(description = "Using mpg:carb mahalanobis distance for each observation is within 30 median absolute deviations from the median",
+              maha_dist, within_n_mads(30), mpg:carb, col_based = TRUE) %>%
+  assert_rows(description = "Using mpg:carb mahalanobis distance for each observation is within 3 median absolute deviations from the median",
+              maha_dist, within_n_mads(3), mpg:carb, col_based = TRUE) %>%
+  assert_if(description = "Column drat has only positive values", drat > 0) %>%
+  assert_if(description = "Column drat has only values larger than 3", drat > 3)
 
-get_results(validator)
+get_results()
 
-save_report(validator)
+save_report()
 browseURL("validation_report.html")
 
-save_report(validator, success = FALSE)
+save_report(success = FALSE)
 browseURL("validation_report.html")
 
-save_report(validator, ui_constructor = render_raw_report_ui)
+save_report(ui_constructor = render_raw_report_ui)
 browseURL("validation_report.html")
 
-save_results(validator, "results.csv")
+save_results("results.csv")
 
-save_summary(validator, "validation_log.txt")
+save_summary("validation_log.txt")
