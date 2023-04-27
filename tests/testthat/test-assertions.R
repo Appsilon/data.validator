a663e8d5d07f19afdcb6f8fe44d06dac95bfb506
Jakub Nowicki <kuba@appsilon.com>
2023-04-27 15:00:12 +0200

    test: Add unit tests for empty columns selection.

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -1,4 +1,4 @@
-context("validator")
+context("assertions")
 
 test_that("get_assert_method correctly choses assertion method", {
   fun_1 <- function() 1
@@ -23,4 +23,50 @@ test_that("Validation works even with evaluation error", {
 
   expect_equal(nrow(validation_result), nrow(iris))
   expect_length(attr(validation_result, "assertr_errors"), 1)
+})
+
+test_that("validate_cols selects all columns if there are no columns selected", {
+  data <- data.frame(V1 = c("c", "d"), V2 = c(2, 2), V3 = c(1, 1))
+
+  result <- validate(data) %>%
+    validate_cols(is.character)
+
+  expect_equal(length(attr(result, which = "assertr_errors")), 2)
+})
+
+test_that("validate_cols throws a message if there are no columns selected", {
+  data <- data.frame(V1 = c("c", "d"), V2 = c(2, 2), V3 = c(1, 1))
+
+  validation <- validate(data)
+
+  expect_message(validate_cols(validation, function(x) TRUE))
+})
+
+test_that("validate_rows selects all columns if there are no columns selected", {
+  data <- data.frame(
+    V1 = c(1, 0, 0),
+    V2 = c(0, 0, 0),
+    V3 = c(0, 1, 0)
+  )
+
+  is_lower_than_one <- function(x) {
+    x < 1
+  }
+
+  result <- validate(data) %>%
+    validate_rows(rowSums, is_lower_than_one)
+
+  expect_equal(nrow(attr(result, "assertr_errors")[[1]]$error_df), 2)
+})
+
+test_that("validate_rows throws a message if there are no columns selected", {
+  data <- data.frame(
+    V1 = c(1, 0, 0),
+    V2 = c(0, 0, 0),
+    V3 = c(0, 1, 0)
+  )
+
+  validation <- validate(data)
+
+  expect_message(validate_rows(validation, rowSums, function(x) TRUE))
 })
