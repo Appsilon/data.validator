b514cad91c80eb7fc31916f9b3a89ec34d176845
Anirban Shaw <anirbanshaw24@gmail.com>
2023-08-10 20:26:09 +0530

    fix: add example and alphabetical WORDLIST

--- examples/save_results_methods/example.R
+++ examples/save_results_methods/example.R
@@ -32,3 +32,6 @@ base_save <- function(x, file, ...) {
 }
 save_results(report, file_name = "results", method = base_save)
 
+# If the function passed to method already has 'x' and 'file' arguments,
+# we do not need a wrapper function.
+save_results(report, method = utils::write.csv)
